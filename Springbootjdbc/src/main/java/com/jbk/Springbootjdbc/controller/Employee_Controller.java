e5c73ee08828ab05fd5dae6d797ec67673fc485b
MonikaShingare <shingare.monika@gmail.com>
2021-05-13 19:54:53 +0530

    Signed-off-by: MonikaShingare <shingare.monika@gmail.com>

--- Springbootjdbc/src/main/java/com/jbk/Springbootjdbc/controller/Employee_Controller.java
+++ Springbootjdbc/src/main/java/com/jbk/Springbootjdbc/controller/Employee_Controller.java
@@ -19,7 +19,7 @@ public class Employee_Controller {
 	@PostMapping("/save-employee")
 	public String saveEmployee(@RequestBody Employee employee)
 	{
-		System.out.println(" hello INDIA");
+		System.out.println(" hello people");
 			String msg=service.saveEmployee(employee);
 		
 			return msg;
